7ba401d255c6ce1561b10632ac92ced73c5467ce
JamesEMerr <jamesEmerr@gmail.com>
2024-02-28 19:23:03 -0600

    Initial

--- issue-tracker/app/page.tsx
+++ issue-tracker/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         />
           <div
           style={{
-            backgroundColor: 'Black',           
+            backgroundColor: 'Blue',           
         }}
         />
       </div>
