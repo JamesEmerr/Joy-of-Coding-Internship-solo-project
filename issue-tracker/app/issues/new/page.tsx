00b7d16d6c21a018aa527aee7c99f8af77443f4a
JamesEMerr <jamesEmerr@gmail.com>
2024-03-07 11:21:34 -0600

    extract error message

--- issue-tracker/app/issues/new/page.tsx
+++ issue-tracker/app/issues/new/page.tsx
@@ -8,14 +8,20 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { createIssueSchema } from "@/app/validationSchemas";
-import { z } from 'zod'
+import { z } from "zod";
+import ErrorMessage from "@/app/components/ErrorMessage";
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
   const router = useRouter();
-  const { register, control, handleSubmit, formState: { errors } } = useForm<IssueForm>({
-    resolver: zodResolver(createIssueSchema)
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
   });
   const [error, setError] = useState("");
 
@@ -40,7 +46,9 @@ const NewIssuePage = () => {
         <TextField.Root>
           <TextField.Input placeholder="Title" {...register("title")} />
         </TextField.Root>
-        {errors.title && <Text color="red" as="p">{errors.title.message}</Text>}
+                  <ErrorMessage>
+            {errors.title?.message}
+            </ErrorMessage>
         <Controller
           name="description"
           control={control}
@@ -48,7 +56,11 @@ const NewIssuePage = () => {
             <SimpleMDE placeholder="Description" {...field} />
           )}
         />
-{errors.description && <Text color="red" as="p">{errors.description.message}</Text>}
+        {errors.description && (
+          <ErrorMessage>
+            {errors.description?.message}
+            </ErrorMessage>
+        )}
         <Button>Submit Issues</Button>
       </form>
     </div>
